00c5f6835e29422e32bdf214c8e0087e10e71760
TusharDimri <tushar.dimri22@gmail.com>
2024-04-23 21:28:53 +0530

    Add Search Functionality

--- Ecom/greatkart/carts/views.py
+++ Ecom/greatkart/carts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from store.models import Product
 from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def _cart_id(request): # Private Function
@@ -38,6 +39,8 @@ def add_cart(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=0):
     try:
+        tax=0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -45,7 +48,7 @@ def cart(request, total=0, quantity=0, cart_items=0):
             quantity = cart_item.quantity
         tax = (2 * total)/100
         grand_total = total + tax
-    except Exception as e: 
+    except ObjectDoesNotExist: 
         pass # ignore
 
     context = {
